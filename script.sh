0932d53d0b489c591d4f6838b82b3872c46e4b64
NaidaDV <62427073+NaidaDV@users.noreply.github.com>
2020-09-08 19:05:25 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,13 +8,3 @@ sudo yum install nano -y
 sudo yum install wget -y
 sudo git clone https://github.com/NaidaDV/for-IaC-HW.git /etc/ansible/roles
 sudo ansible-playbook --connection=local --inventory 127.0.0.1, /etc/ansible/roles/playbook.yml
-
-
-#sudo git clone https://github.com/geerlingguy/ansible-role-nginx.git /etc/ansible/roles/geerlingguy.nginx
-#sudo ansible-playbook --connection=local --inventory 127.0.0.1, /etc/ansible/roles/geerlingguy.nginx/molecule/default/converge.yml
-
-#sudo git clone https://github.com/geerlingguy/ansible-role-php.git /etc/ansible/roles/geerlingguy.php
-#sudo git clone https://github.com/geerlingguy/ansible-role-git.git /etc/ansible/roles/geerlingguy.git
-#sudo git clone https://github.com/geerlingguy/ansible-role-repo-remi.git /etc/ansible/roles/geerlingguy.repo-remi
-
-#sudo ansible-playbook --connection=local --inventory 127.0.0.1, /etc/ansible/roles/geerlingguy.php/molecule/default/converge.yml
